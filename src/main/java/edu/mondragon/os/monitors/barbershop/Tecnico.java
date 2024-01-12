4d81b5c154f35674a17865843ac5687c2ede62cd
lidealvarez <lide.alvarez@alumni.mondragon.edu>
2024-01-12 10:24:25 +0100

    aldaketa main kendu

--- src/main/java/edu/mondragon/os/monitors/barbershop/Tecnico.java
+++ src/main/java/edu/mondragon/os/monitors/barbershop/Tecnico.java
@@ -14,8 +14,6 @@ public class Tecnico extends Thread {
     }
 
 
-    
-
     public synchronized void repararValvulasConAnomalia() {
         for (int i = 0; i < sincronizacion.getNumValvulas(); i++) {
             try {
