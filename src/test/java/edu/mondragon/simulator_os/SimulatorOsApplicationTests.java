9903d17b1b5cadb414ea564a802ce14e4110c544
lidealvarez <lide.alvarez@alumni.mondragon.edu>
2024-01-18 11:42:39 +0100

    test main4

--- src/test/java/edu/mondragon/simulator_os/SimulatorOsApplicationTests.java
+++ src/test/java/edu/mondragon/simulator_os/SimulatorOsApplicationTests.java
@@ -2,10 +2,11 @@ package edu.mondragon.simulator_os;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
 class SimulatorOsApplicationTests {
 
 	@Test
@@ -19,6 +20,37 @@ class SimulatorOsApplicationTests {
         for (Valve valve : app.valves) {
             assertEquals(Thread.State.RUNNABLE, valve.getState());
         }
+    }
+
+	@Test
+    void testWaitEndOfThreads() throws InterruptedException {
+        SimulatorOsApplication app = new SimulatorOsApplication();
+        app.startThreads();
+        app.waitEndOfThreads();
+
+        // Verificar que los hilos se hayan detenido correctamente
+        assertEquals(Thread.State.TERMINATED, app.worker.getState());
+
+        for (Valve valve : app.valves) {
+            assertEquals(Thread.State.TERMINATED, valve.getState());
+        }
+    }
+
+	@Test
+    void testMain() {
+        // Redirigir la salida estándar para capturarla en un String
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        // Ejecutar el método main
+        SimulatorOsApplication.main(new String[]{});
+
+        // Restaurar la salida estándar
+        System.setOut(System.out);
+
+        // Obtener la salida como String y verificarla
+        String output = outputStream.toString().trim();
+        assertEquals("Tu mensaje de salida esperado", output);
     }
 
 
