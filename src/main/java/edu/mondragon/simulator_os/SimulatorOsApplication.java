cf91f256038eaea7958af47f25aba0ea3c5d86da
Uxue <uxue.telleria@alumni.mondragon.edu>
2024-01-18 15:50:42 +0100

    sonarlinteko errorek

--- src/main/java/edu/mondragon/simulator_os/SimulatorOsApplication.java
+++ src/main/java/edu/mondragon/simulator_os/SimulatorOsApplication.java
@@ -2,12 +2,14 @@ package edu.mondragon.simulator_os;
 
 public class SimulatorOsApplication {
     static final int NUMVALVES = 15;
-    protected static Management management;
+    private static Management management;
     protected Worker worker;
     protected Valve[] valves;
+    static {
+        management = new Management();
+    }
 
     public SimulatorOsApplication() {
-        management = new Management();
         worker = new Worker(management);
         valves = new Valve[NUMVALVES];
         for (int i = 0; i < NUMVALVES; i++) {
@@ -22,30 +24,36 @@ public class SimulatorOsApplication {
         }
     }
 
+    @SuppressWarnings("java:S106")
     public void waitEndOfThreads() {
         try {
+            // Esperar a que todas las válvulas terminen
             for (Valve customer : valves) {
                 customer.join();
             }
+    
+            // Interrumpir al trabajador y esperar a que termine
             worker.interrupt();
-
-            try {
-                worker.join();
-
-            } catch (InterruptedException e) {
-
-                Thread.currentThread().interrupt();
-            }
+            worker.join();
         } catch (InterruptedException e) {
+            // Manejar la interrupción aquí o propagarla a niveles superiores
+            Thread.currentThread().interrupt();
+            // Loggear o imprimir información de la excepción, si es necesario
+            System.err.println("Error al esperar que los hilos finalicen: " + e.getMessage());
         }
     }
-
+    
+    @SuppressWarnings("java:S106")
     public static void main(String[] args) {
-
-        SimulatorOsApplication app = new SimulatorOsApplication();
-
-        app.startThreads();
-        app.waitEndOfThreads();
-        System.out.println(management.getTotalRepairTimeandBadValves());
+        try {
+            SimulatorOsApplication app = new SimulatorOsApplication();
+            app.startThreads();
+            app.waitEndOfThreads();
+            System.out.println(management.getTotalRepairTimeandBadValves());
+        } catch (Exception e) {
+            // Registra la excepción en un sistema de registro
+            e.printStackTrace();
+        }
     }
+    
 }
