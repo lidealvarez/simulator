c957ec062967de42814a1446ca949928cd2d1fae
lidealvarez <lide.alvarez@alumni.mondragon.edu>
2024-01-21 21:04:54 +0100

    aldaketak sonarbe passed jartzeko

--- src/main/java/edu/mondragon/simulator_os/TestUtils.java
+++ src/main/java/edu/mondragon/simulator_os/TestUtils.java
@@ -7,12 +7,25 @@ public class TestUtils {
     private static final PrintStream originalOut = System.out;
     private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
+    // Definir una excepción dedicada para manejar errores en TestUtils
+    public static class ConsoleOutputException extends RuntimeException {
+        public ConsoleOutputException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+
     public static void setUpConsoleOutput() {
         System.setOut(new PrintStream(outContent));
     }
 
     public static String getConsoleOutput() {
-        System.setOut(originalOut);
-        return outContent.toString().trim();
+        try {
+            return outContent.toString("UTF-8").trim();
+        } catch (Exception e) {
+            // Lanzar una excepción específica en lugar de RuntimeException genérica
+            throw new ConsoleOutputException("Error al convertir la salida de la consola a cadena", e);
+        } finally {
+            System.setOut(originalOut);
+        }
     }
 }
--- src/test/java/edu/mondragon/simulator_os/ManagementTest.java
+++ src/test/java/edu/mondragon/simulator_os/ManagementTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ManagementTest {
@@ -36,8 +35,5 @@ public class ManagementTest {
         String actualOutput = TestUtils.getConsoleOutput();
         assertTrue(actualOutput.contains(expectedOutput));
     }
-
-    // Otros métodos de prueba para otros casos...
-
     
 }
