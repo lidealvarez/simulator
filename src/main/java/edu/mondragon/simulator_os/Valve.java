b14e4a12b1b543a6fc4f61f10609fd4d7d3481de
lidealvarez <lide.alvarez@alumni.mondragon.edu>
2024-01-22 22:07:09 +0100

    bilera ta gero 2

--- src/main/java/edu/mondragon/simulator_os/Valve.java
+++ src/main/java/edu/mondragon/simulator_os/Valve.java
@@ -5,45 +5,56 @@ import java.util.Random;
 
 public class Valve extends Thread {
 
-    private int arrivalTime = 0;
     private Management management;
     private Random rand;
-//arrayList (denbora, zenbatAldizAnomalia)
-    //semaforoa 
-    
-    public Valve(int id, Management management) {
-        super("Valve " + id);
+    private int valveId;
+    private int pressure;
+
+  
+    public Valve(int valveId, Management management) {
+        super("Valve ");
+        this.valveId = valveId;
         this.management = management;
-        this.arrivalTime += 500 * id;
         rand = new SecureRandom();
     }
 
-    // Getter para arrivalTime
-    public int getArrivalTime() {
-        return arrivalTime;
-    }
-
-    // Getter para management
+    
     public Management getManagement() {
         return management;
     }
 
-    // Getter para rand
     public Random getRandom() {
         return rand;
     }
 
-    @SuppressWarnings("java:S106")
+    public int getValveId() {
+        return valveId;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
     @Override
     public void run() {
-        try {
-            Thread.sleep(this.arrivalTime);
-            management.writePressure(getName(), rand.nextInt(15));
-        } catch (NullPointerException e) {
-            System.out.println(e.toString());
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        while (!this.isInterrupted()) {
+            try {
+
+                Thread.sleep(rand.nextInt(10000));
+                pressure=rand.nextInt(15);
+                management.writePressure(this);
+            } catch (InterruptedException e) {
+                this.interrupt();
+            }
         }
     }
-}//while bat sartu, balbularen bizitza si esta bien no pasa nda, gaizki baldin banadgo acquire itet eta workerra libre daonian release ingoit 
-//zenbat aldiz apurtu, eta zenbat denbora tardatu dan konpontzen aldi bakoitzian
+
+}
+// while bat sartu, balbularen bizitza si esta bien no pasa nda, gaizki baldin
+// banadgo acquire itet eta workerra libre daonian release ingoit
+// zenbat aldiz apurtu, eta zenbat denbora tardatu dan konpontzen aldi
+// bakoitzian
--- src/test/java/edu/mondragon/simulator_os/ValveTests.java
+++ src/test/java/edu/mondragon/simulator_os/ValveTests.java
@@ -2,19 +2,17 @@ package edu.mondragon.simulator_os;
 
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
- class ValveTests {
+class ValveTests {
 
     @Test
-     void testValveConstructorAndGetters() {
+    void testValveConstructorAndGetters() {
         Management management = new Management();
         Valve valve = new Valve(1, management);
 
         // Verifica que el constructor inicializa correctamente las propiedades
         assertEquals("Valve 1", valve.getName());
-        assertEquals(500, valve.getArrivalTime()); // asumiendo que arrivalTime debería ser 500 * id
         assertEquals(management, valve.getManagement());
         assertNotNull(valve.getRandom());
     }
@@ -25,10 +23,8 @@ import static org.junit.jupiter.api.Assertions.*;
         Valve valve = new Valve(1, null);
 
         assertEquals("Valve 1", valve.getName());
-        assertEquals(500, valve.getArrivalTime());
+
         assertNull(valve.getManagement());
         assertNotNull(valve.getRandom());
     }
 }
-
-
